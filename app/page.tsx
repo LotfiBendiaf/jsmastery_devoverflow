8b7cf7755c8ce088f6393ddf81a57f40322e96d6
Lotfi <lotfi.m.bendiaf@gmail.com>
2024-11-04 17:58:46 +0100

    Tailwind CSS Setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl font-black text-blue-500">
-        Welcome to the course!
-      </h1>
+      <h1 className="h1-bold">Welcome to the course!</h1>
     </div>
   );
 };
