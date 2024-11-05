ef78278009d87857a36b22848d521963a83b1bef
Lotfi <lotfi.m.bendiaf@gmail.com>
2024-11-05 01:46:07 +0100

    Assets & Metadata

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ const Home = () => {
   return (
     <div>
       <h1 className="h1-bold">Welcome to the course!</h1>
+      <h1 className="h1-bold font-space-grotesk">
+        Welcome to the course! (Grotesk Font gym)
+      </h1>
     </div>
   );
 };
