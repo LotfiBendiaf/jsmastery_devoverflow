400eb4ccaae425b43242741be9010dfc1b7cc6a4
Lotfi <lotfi.m.bendiaf@gmail.com>
2024-11-29 12:47:54 +0100

    Set up Light & Dark Theme, Shadcn, Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="h1-bold">Welcome to the course!</h1>
-      <h1 className="h1-bold font-space-grotesk">
-        Welcome to the course! (Grotesk Font gym)
+    <div className="flex flex-col items-center justify-center gap-5">
+      <Image
+        src="/images/site-logo.svg"
+        width={500}
+        height={500}
+        alt="Picture of the author"
+      />
+      <h1 className="h1-bold">Welcome to the NextJs Project</h1>
+      <h1 className="h1-bold font-space-grotesk text-blue-700">
+        It is a Boilerplate project
       </h1>
     </div>
   );
