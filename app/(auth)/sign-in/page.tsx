96630653e9840fff9351b51ea54973ec23d2c424
Lotfi <lotfi.m.bendiaf@gmail.com>
2025-01-16 22:50:37 +0100

    Implement Sign In Credentials

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import AuthForm from "@/components/forms/AuthForm";
 import { SignInSchema } from "@/lib/validations";
 import SocialAuthForm from "@/components/forms/SocialAuthForm";
+import { signInWithCredentials } from "@/lib/actions/auth.action";
 
 const SignIn = () => {
   return (
@@ -12,7 +13,7 @@ const SignIn = () => {
         formType="SIGN_IN"
         schema={SignInSchema}
         defaultValues={{ email: "", password: "" }}
-        onSubmit={(data) => Promise.resolve({ success: true, data })}
+        onSubmit={signInWithCredentials}
       />
       <div className="border-b-[1px] h-1 my-6 flex items-center justify-center">
         <span className="text-light400_light500 px-2 background-light800_dark200">
